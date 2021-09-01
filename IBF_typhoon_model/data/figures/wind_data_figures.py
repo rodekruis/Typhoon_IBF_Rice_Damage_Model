54be695d6d2a01fba79afb5c676852af29d7d263
mariekevanbrussel <58587180+mariekevanbrussel@users.noreply.github.com>
2021-09-01 23:27:33 +0200

    updated wind data collection

--- IBF_typhoon_model/data/figures/wind_data_figures.py
+++ IBF_typhoon_model/data/figures/wind_data_figures.py
@@ -21,18 +21,24 @@ os.chdir("C:\\Users\\Marieke\\GitHub\\Typhoon_IBF_Rice_Damage_Model")
 cdir = os.getcwd()
 
 #%% Loading data
-file_name = "IBF_typhoon_model\\data\\wind_data\\output\\historical_typhoons_wind_test.csv"
+file_name = (
+    "IBF_typhoon_model\\data\\wind_data\\output\\nock-ten2011_windgrid_output.csv"
+)
 path = os.path.join(cdir, file_name)
 df_wind = pd.read_csv(path)
 
-file_name = "IBF_typhoon_model\\data\\phl_administrative_boundaries\\phl_admbnda_adm3.shp"
+file_name = (
+    "IBF_typhoon_model\\data\\phl_administrative_boundaries\\phl_admbnda_adm3.shp"
+)
 path = os.path.join(cdir, file_name)
 df_admin = gpd.read_file(path)
 
 
 # %%
 
-df_combined = pd.merge(df_admin, df_wind, how='left', left_on=['ADM3_PCODE'], right_on = ['adm3_pcode'])
+df_combined = pd.merge(
+    df_admin, df_wind, how="left", left_on=["ADM3_PCODE"], right_on=["adm3_pcode"]
+)
 # # %%
 
 
@@ -53,7 +59,7 @@ df_admin.plot(ax=ax, color="whitesmoke", zorder=1)
 
 df_combined.plot(
     ax=ax,
-    column='vmax_sust',
+    column="vmax_sust",
     cmap="Reds",
     vmin=0,
     vmax=65,
@@ -62,7 +68,7 @@ df_combined.plot(
     # cax=cax,
 )
 
-path = os.path.join(cdir, 'IBF_typhoon_model\\data\\figures\\wind_figure.png')
+path = os.path.join(cdir, "IBF_typhoon_model\\data\\figures\\wind_figure_new.png")
 
 fig.savefig(path)
 
