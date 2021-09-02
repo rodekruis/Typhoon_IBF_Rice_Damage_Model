f51ee030d627da7279725831477cd6e4d00ea98c
mariekevanbrussel <58587180+mariekevanbrussel@users.noreply.github.com>
2021-09-02 12:46:59 +0200

    Updated script
    Reflecting the new data and adding a new circular threshold

--- IBF_typhoon_model/data/finding_zeros.py
+++ IBF_typhoon_model/data/finding_zeros.py
@@ -6,9 +6,13 @@ import openpyxl
 import os
 import matplotlib.pyplot as plt
 
-#%% Loading data
+os.chdir("C:\\Users\\Marieke\\GitHub\\Typhoon_IBF_Rice_Damage_Model")
 cdir = os.getcwd()
-file_name = "data\\combined_input_data\\input_data.xlsx"
+
+#%% Loading data
+file_name = (
+    "IBF_typhoon_model\\data\\restricted_data\\combined_input_data\\input_data.xlsx"
+)
 path = os.path.join(cdir, file_name)
 df = pd.read_excel(path, engine="openpyxl")
 df.head()
@@ -19,24 +23,29 @@ df["perc_loss"] = df["perc_loss"].replace(0, np.nan)
 #%% create new boolean column for missing loss data
 df["no_loss_data"] = df["perc_loss"].apply(lambda x: pd.isna(x))
 
-#%%
-df = df[df['vmax_sust'] < 50]
+#%% Remove all observations with track distance > 500
+df_plot = df[df["dis_track_min"].notnull()]
+
+#%% Reduce the data size for plotting  only relevant part
+df_plot_new = df_plot[(df_plot["vmax_sust"] < 80) & (df_plot["rainfall_max"] < 200)]
 
 #%% plot 2D distribution of wind and rainfall
-sns.displot(df, x="rainfall_max", y="vmax_sust", hue="no_loss_data")
+# sns.displot(df_plot, x="rainfall_max", y="vmax_sust", hue="no_loss_data")
+# plt.show()
+
+sns.displot(df_plot_new, x="rainfall_max", y="vmax_sust", hue="no_loss_data")
 
 # add circular threshold
 xarr, yarr = [], []
-for i in range(101):
-    xs = i * 2
-    y = np.sqrt(200 ** 2 - xs ** 2)
-    ys = y * 0.1
+for i in range(200):
+    xs = i
+    y = np.sqrt(150 ** 2 - xs ** 2) * 0.4
+    # ys = y * 0.4
+    ys = y
     xarr.append(xs)
     yarr.append(ys)
 
 sns.lineplot(x=xarr, y=yarr)
-# plt.ylim(-1, 25)
-# plt.xlim(-1, 100)
 plt.show()
 
 #%% Make similar plot but now with distance
